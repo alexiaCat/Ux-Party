b5c6fc91135306d2618b6dbac5286bf71a3a54c1
alexiaCat <alexia_12ceci@hotmail.com>
2022-10-13 21:34:55 -0300

    gamehandler

--- gamehandler.gd
+++ gamehandler.gd
@@ -2,8 +2,20 @@ extends Node
 
 var vidas_jugador = 5
 var time_left = 60
-var puntos = 0
+var puntajeglobal
 var turno = true
+var pnivel1 
+var pnivel2 
+var pnivel3 
+var pnivel4 
+var pnivel5
+var pnivel6 
+var pnivel7 
+var pnivel8
+var pnivel9
+var puntajeahorcado = 500
+var puntajequiz = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- main/Main.gd
+++ main/Main.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 func _on_Play_button_pressed():
 	get_tree().change_scene("res://main_map/Main_scene.tscn")
+
+
+func _on_Config_button_pressed():
+	get_tree().change_scene("res://menu_options.tscn")
