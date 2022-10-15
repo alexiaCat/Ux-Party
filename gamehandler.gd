249b73e34a5c792558181c5ad60d9d95c8191256
alexiaCat <alexia_12ceci@hotmail.com>
2022-10-14 23:42:32 -0300

    mapa ok, vidas ok

--- gamehandler.gd
+++ gamehandler.gd
@@ -2,7 +2,7 @@ extends Node
 
 var vidas_jugador = 5
 var time_left = 60
-var puntajeglobal
+var puntajeglobal = 0
 var turno = true
 var pnivel1 
 var pnivel2 
@@ -13,13 +13,26 @@ var pnivel6
 var pnivel7 
 var pnivel8
 var pnivel9
-var puntajeahorcado = 500
-var puntajequiz = 0
+#puntaje inicial de los minijuegos
+const puntajeahorcado = 500
+const puntajequiz = 0
+#variables para saber si el nivel esta pasado o no, ej: no poder ingresar a nivel 2 sin pasar nivel 1
+var level1 = false
+var level2 = false
+var level3 = false
+var level4 = false
+var level5 = false
+var level6 = false
+var level7 = false
+var level8 = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
+func update_vidas():
+	get_tree().get_nodes_in_group("vidasjugador")[0].text = String(vidas_jugador)
 
 func update_time():
 	get_tree().get_nodes_in_group("tiempo")[0].text = String(time_left/60) + ":" + String(time_left%60)
--- main_map/Main_scene.gd
+++ main_map/Main_scene.gd
@@ -1,20 +1,67 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	Gamehandler.update_vidas()
 
 
 func _on_Level1_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene("res://hanged_game/Main_hanged_game.tscn")
+
+
+func _on_Level2_pressed():
+	if(Gamehandler.level1 == false):
+		print("no puedes ingresar")
+	else:
+		get_tree().change_scene("res://hanged_game/Main_hanged_game.tscn")
+
+
+func _on_Level3_pressed():
+	if(Gamehandler.level2 == false):
+		print("no puedes ingresar")
+	else:
+		get_tree().change_scene("res://hanged_game/Main_hanged_game.tscn")
+
+
+func _on_Level4_pressed():
+	if(Gamehandler.level3 == false):
+		print("no puedes ingresar")
+	else:
+		get_tree().change_scene("res://hanged_game/Main_hanged_game.tscn")
+
+
+func _on_Level5_pressed():
+	if(Gamehandler.level4 == false):
+		print("no puedes ingresar")
+	else:
+		get_tree().change_scene("res://hanged_game/Main_hanged_game.tscn")
+
+
+func _on_Level6_pressed():
+	if(Gamehandler.level5 == false):
+		print("no puedes ingresar")
+	else:
+		get_tree().change_scene("res://hanged_game/Main_hanged_game.tscn")
+
+
+func _on_Level7_pressed():
+	if(Gamehandler.level6 == false):
+		print("no puedes ingresar")
+	else:
+		get_tree().change_scene("res://hanged_game/Main_hanged_game.tscn")
+
+
+func _on_Level8_pressed():
+	if(Gamehandler.level7 == false):
+		print("no puedes ingresar")
+	else:
+		get_tree().change_scene("res://hanged_game/Main_hanged_game.tscn")
+
+
+func _on_Level9_pressed():
+	if(Gamehandler.level8 == false):
+		print("no puedes ingresar")
+	else:
+		get_tree().change_scene("res://hanged_game/Main_hanged_game.tscn")
