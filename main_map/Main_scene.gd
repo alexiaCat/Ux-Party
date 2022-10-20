8009746153df32d7b8b328e81a73f9a151237bec
alexiaCat <alexia_12ceci@hotmail.com>
2022-10-20 04:06:05 -0300

    gamehandler

--- main_map/Main_scene.gd
+++ main_map/Main_scene.gd
@@ -158,56 +158,56 @@ func _on_Level1_pressed():
 
 
 func _on_Level2_pressed():
-	if(Gamehandler.level1 == false):
+	if(Gamehandler.vidas_jugador < 0 || Gamehandler.level1 == false):
 		print("no puedes ingresar")
 	else:
 		get_tree().change_scene("res://hanged_game/Main_hanged_game.tscn")
 
 
 func _on_Level3_pressed():
-	if(Gamehandler.level2 == false):
+	if(Gamehandler.vidas_jugador > 0 || Gamehandler.level2 == false):
 		print("no puedes ingresar")
 	else:
 		get_tree().change_scene("res://hanged_game/Main_hanged_game.tscn")
 
 
 func _on_Level4_pressed():
-	if(Gamehandler.level3 == false):
+	if(Gamehandler.vidas_jugador > 0 || Gamehandler.level3 == false):
 		print("no puedes ingresar")
 	else:
 		get_tree().change_scene("res://hanged_game/Main_hanged_game.tscn")
 
 
 func _on_Level5_pressed():
-	if(Gamehandler.level4 == false):
+	if(Gamehandler.vidas_jugador > 0 || Gamehandler.level4 == false):
 		print("no puedes ingresar")
 	else:
 		get_tree().change_scene("res://hanged_game/Main_hanged_game.tscn")
 
 
 func _on_Level6_pressed():
-	if(Gamehandler.level5 == false):
+	if(Gamehandler.vidas_jugador > 0 || Gamehandler.level5 == false):
 		print("no puedes ingresar")
 	else:
 		get_tree().change_scene("res://hanged_game/Main_hanged_game.tscn")
 
 
 func _on_Level7_pressed():
-	if(Gamehandler.level6 == false):
+	if(Gamehandler.vidas_jugador > 0 || Gamehandler.level6 == false):
 		print("no puedes ingresar")
 	else:
 		get_tree().change_scene("res://hanged_game/Main_hanged_game.tscn")
 
 
 func _on_Level8_pressed():
-	if(Gamehandler.level7 == false):
+	if(Gamehandler.vidas_jugador > 0 || Gamehandler.level7 == false):
 		print("no puedes ingresar")
 	else:
 		get_tree().change_scene("res://hanged_game/Main_hanged_game.tscn")
 
 
 func _on_Level9_pressed():
-	if(Gamehandler.level8 == false):
+	if(Gamehandler.vidas_jugador > 0 || Gamehandler.level8 == false):
 		print("no puedes ingresar")
 	else:
 		get_tree().change_scene("res://hanged_game/Main_hanged_game.tscn")
