eb847a4c34f6e8b9d8ed843ffac22ba12cf27971
alexiaCat <alexia_12ceci@hotmail.com>
2022-11-17 20:24:23 -0300

    apk actualizada

--- menus/menu_comparar/Menu_cyd.gd
+++ menus/menu_comparar/Menu_cyd.gd
@@ -47,12 +47,13 @@ func _on_Button_quit_pressed():
 
 func _on_btn_exit_button_up():
 	popup.visible = false
+	Gamehandler.guardar_volumen()
 	get_tree().get_nodes_in_group("botoncyd")[0].disabled = false
 	get_tree().get_nodes_in_group("botoncyd")[1].disabled = false
 	get_tree().get_nodes_in_group("cydtuto")[0].disabled = false
 	get_tree().get_nodes_in_group("timercyd")[0].start()
 	get_tree().get_nodes_in_group("tiemporespuesta")[0].start()
-	Userdata.load_config()
+	
 
 
 func _on_Salir_button_up():
